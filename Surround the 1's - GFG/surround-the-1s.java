7537f7e04b9d7f1f810e3c4f842fa4a050667075
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-08-22 13:29:08 +0530

    Added solution - LeetHub

---
+++ Surround the 1's - GFG/surround-the-1s.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] s = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int m = Integer.parseInt(s[1]);
+            int[][] matrix = new int[n][m];
+            for(int i = 0; i < n; i++){
+                String[] S = br.readLine().trim().split(" ");
+                for(int j = 0; j < m; j++)
+                    matrix[i][j] = Integer.parseInt(S[j]);
+            }
+            Solution ob = new Solution();
+            int ans = ob.Count(matrix);
+            out.println(ans);
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int  Count(int[][] matrix)
+    {
+        // code here
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int ans=0;
+        int dx[]={1,-1,0,0,1,1,-1,-1};
+        int dy[]={0,0,1,-1,1,-1,1,-1};
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==1){
+                int cnt=0;
+                for(int k=0;k<8;k++){
+                    int nx=i+dx[k];
+                    int ny=j+dy[k];
+                    if(nx>=0 && nx<n && ny>=0 && ny<m && matrix[nx][ny]==0){
+                        cnt++;
+                    }
+                }
+                if(cnt!=0 && cnt%2==0) ans++;
+            }
+        }
+    }
+    return ans;
+}
+}
