29cb98663e8f1e7a3200ef8190c983a31d85cd69
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-08-26 21:49:18 +0530

    Time: 3 ms (25.15%), Space: 55.3 MB (69.69%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,19 +1,17 @@
 class Solution {
     public int maxScore(int[] A, int B) {
          int n=A.length;
-       int psum[]=new int[n]; // prefix sum
-       int ssum[]=new int[n];
-       psum[0]=A[0];
-       for(int i=1;i<n;i++) psum[i]=psum[i-1]+A[i];
-
-       ssum[n-1]=A[n-1];
-       for(int i=n-2;i>=0;i--) ssum[i]=ssum[i+1]+A[i];
-
-       int maxi=Math.max(psum[B-1],ssum[n-B]); // zero selection from end and zero selection from start
-
-       for(int i=1;i<B;i++){
-           maxi=Math.max(maxi,psum[i-1]+ssum[n-(B-i)]);
+       int psum=0;// prefix sum
+       int ssum=0;
+       
+       for(int i=n-1;i>=n-B;i--) ssum+=A[i]; // B selection from end 0 selection from start
+       int maxi=ssum;
+       for(int i=0;i<B;i++){
+           psum+=A[i];
+           ssum-=A[n-(B-i)];
+           maxi=Math.max(ssum+psum, maxi);
        }
+
        return maxi;
     }
 }
