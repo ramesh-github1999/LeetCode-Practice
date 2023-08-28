d34f68b77f95d21f927845b6afa260cff2c23871
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-08-28 21:47:09 +0530

    Time: 7 ms (95.93%), Space: 19.8 MB (54.17%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int n=height.size();
-        // find left_max and right max , take its minium and subtract with hight[i] 
-        if(n==0) return 0;
-     
-        vector<int>left(n),right(n);
-        left[0]=height[0];
-        for(int i=1;i<n;i++){
-            left[i]=max(left[i-1],height[i]);
+    int trap(vector<int>& A) {
+        int n=A.size();
+    int i=0,j=n-1;
+    int maxL=0,maxR=0;
+    int ans=0;
+    while(i<j){
+        if(A[i]<=A[j]){
+            if(maxL<=A[i]) maxL=A[i];
+            else ans+=maxL-A[i];
+            i++;
         }
-        right[n-1]=height[n-1];
-        for(int i=n-2;i>=0;i--)
-            right[i]=max(right[i+1],height[i]);
-        
-        int ans=0;
-        for(int i=0;i<n;i++){
-            ans+=min(left[i],right[i])-height[i];
+        else{
+            if(maxR<=A[j]) maxR=A[j];
+            else ans+=maxR-A[j];
+            j--;
         }
-        return ans;
+    }
+    return ans;
     }
 };
 
