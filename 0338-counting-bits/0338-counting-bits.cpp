4598310a95acb9548a4a37996c702c36c2e90719
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-09-01 10:15:03 +0530

    Time: 3 ms (89.82%), Space: 8 MB (45.56%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
-    int cntBits(int n){
-        int sum=0;
-        while(n!=0){
-            n&=n-1;
-            sum++;   
-        }
-        return sum;
-    }
     vector<int> countBits(int n) {
         vector<int>v(n+1);
         v[0]=0;
         for(int i=1;i<=n;i++){
-            v[i]=cntBits(i);
+            v[i]=v[i/2]+(i%2);
         }
         return v;
     }
