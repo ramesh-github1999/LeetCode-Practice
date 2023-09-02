ce29d1ee6c85e0fbbfaa5bd2bcfaf6be2f9e4b96
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-09-02 23:40:16 +0530

    Time: 71 ms (96.81%), Space: 64.7 MB (77.43%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int solve(string s,unordered_map<string,int>&mp,int idx,vector<int>&dp){
-        if(idx>=s.length()) return 0;
-        if(dp[idx]!=-1) return dp[idx];
-        string currStr="";
-        int minExtra=s.length();
-        for(int currIdx=idx;currIdx<s.length();currIdx++){
-            currStr.push_back(s[currIdx]);
-            int currExtra=(mp.count(currStr))?0:currStr.size();
-            int nextExtra=solve(s,mp,currIdx+1,dp);
-            int totalExtra=currExtra+nextExtra;
-            minExtra=min(totalExtra,minExtra);
-        }
-        return dp[idx]=minExtra;
-    }
+   
     int minExtraChar(string s, vector<string>& dictionary) {
         unordered_map<string,int>mp;
         for(auto i:dictionary)mp[i]++;
         int n=s.length();
-        vector<int>dp(n,-1);
-        return solve(s,mp,0,dp);
+        vector<int>dp(n+1,0);
+        
+        for(int idx=n-1;idx>=0;idx--){
+            string currStr="";
+            int minExtra=s.length();
+            for(int currIdx=idx;currIdx<n;currIdx++){
+                currStr.push_back(s[currIdx]);
+            int currExtra=(mp.count(currStr))?0:currStr.size();
+            int nextExtra=dp[currIdx+1];
+            int totalExtra=currExtra+nextExtra;
+            minExtra=min(totalExtra,minExtra);
+            }
+            dp[idx]=minExtra;
+        }
+        return dp[0];
            
     }
 };
