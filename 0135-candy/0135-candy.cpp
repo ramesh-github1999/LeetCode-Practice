1fb06658039de7f299556fcde1bbe0ff71830567
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-09-13 18:38:24 +0530

    Time: 22 ms (32.22%), Space: 18.6 MB (9.92%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        vector<int>l(n,1),r(n,1);
+         for(int i=1;i<n;i++) {
+             if(ratings[i] >ratings[i-1]) l[i]=l[i-1]+1;
+        }
+         for(int i=n-2;i>=0;i--) {
+             if(ratings[i] >ratings[i+1]) r[i]=r[i+1]+1;
+        }
+        int cnt=0;
+        for(int i=0;i<n;i++) cnt+=max(l[i],r[i]);
+        return cnt;
+    }
+};
