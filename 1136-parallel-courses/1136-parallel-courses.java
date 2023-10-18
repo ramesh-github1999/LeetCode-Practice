ee2f151d189538c2551c2de104fe086f127491e6
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-10-18 13:05:19 +0530

    Time: 9 ms (67.96%), Space: 45.4 MB (50.82%) - LeetHub

---
+++ 1136-parallel-courses/1136-parallel-courses.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minimumSemesters(int n, int[][] relations){
+        int []indegree=new int [n+1];
+        ArrayList<ArrayList<Integer>> graph=new ArrayList<>(n+1);
+        for(int i=0;i<=n;i++) graph.add(new ArrayList<>());
+        for(int i=0;i<relations.length;i++){
+            indegree[relations[i][1]]++;
+            graph.get(relations[i][0]).add(relations[i][1]);
+        }
+        Queue<Integer>q=new LinkedList<>();
+        for(int i=1;i<=n;i++) if(indegree[i]==0) q.add(i);
+        int ans=0;
+        while (q.isEmpty()==false){
+            ans++;
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                int curr=q.poll();
+                List<Integer>l=graph.get(curr);
+                for(int j=0;j<l.size();j++){
+                    indegree[l.get(j)]--;
+                    if(indegree[l.get(j)]==0) q.add(l.get(j));
+                }
+            }
+        }
+        for(int i=1;i<=n;i++) if(indegree[i]!=0) return -1;
+        return ans;
+
+    } 
+    
+}
