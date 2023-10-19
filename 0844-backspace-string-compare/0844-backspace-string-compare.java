5bae87bdaab453a0c045062240a6b38f3fcb71df
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-10-19 11:55:04 +0530

    Time: 7 ms (5.74%), Space: 41.4 MB (6.26%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,19 +1,21 @@
 class Solution {
-public:
-    bool backspaceCompare(string s, string t) {
-        string a="",b="";
+    public boolean backspaceCompare(String s, String t) {
+        String a="";
+        String b="";
         for(int i=0;i<s.length();i++){
-            if(s[i]!='#') a+=s[i];
-            else {
-                if(a.length()>0) a.pop_back();
+            if(s.charAt(i)!='#') a+=s.charAt(i);
+            else{
+                if(a.length()>0) a=a.substring(0,a.length()-1);
             }
         }
         for(int i=0;i<t.length();i++){
-            if(t[i]!='#') b+=t[i];
+            if(t.charAt(i)!='#') b+=t.charAt(i);
             else{
-                if(b.length()>0) b.pop_back();
-            } 
+                if(b.length()>0) b=b.substring(0,b.length()-1);
+            }
         }
-        return a==b;
+
+        if(a.equals(b)) return true;
+        else return false;
     }
-};
+}
