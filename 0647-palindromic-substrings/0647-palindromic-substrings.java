a226d12405e1343b86dbfb84438f6494343da903
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-10-27 16:22:02 +0530

    Time: 831 ms (5.02%), Space: 268.2 MB (5.76%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,15 +1,21 @@
 class Solution {
+    static Map<String,Boolean> mp=new HashMap<>();
     public static boolean isPalindrome(String s){
+         if(mp.containsKey(s)) return mp.get(s);
+        
         int n=s.length();
         for(int i=0;i<n/2;i++){
-            if(s.charAt(i)!=s.charAt(n-1-i))return false;
+            if(s.charAt(i)!=s.charAt(n-1-i)){
+                mp.put(s,false);
+                return false;
+            }
         }
+        mp.put(s,true);
         return true;
     }
     public int countSubstrings(String s) {
         String que="";
-      //  for(int i=0;i<1000;i++) que+='a';
-       // System.out.println(que);
+     
          int n=s.length();
         int cnt=0;
         for (int i = 0; i < n; i++) {
