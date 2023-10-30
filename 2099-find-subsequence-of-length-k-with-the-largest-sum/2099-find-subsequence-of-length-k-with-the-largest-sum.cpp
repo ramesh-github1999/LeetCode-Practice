df980935af9da017906a8432db7d99790a18ce2b
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-10-30 17:25:27 +0530

    Time: 4 ms (76.06%), Space: 9.4 MB (94.15%) - LeetHub

--- 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.cpp
+++ 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     vector<int> maxSubsequence(vector<int>& nums, int k) {
-        
-        vector<int>nums2=nums,ans;
-        sort(nums2.begin(),nums2.end());
-       for(int i=nums2.size()-k;i<nums2.size();i++) ans.push_back(nums2[i]);
-        vector<int>res;
-        for(int i=0;i<nums.size();i++){
-             auto it = find(ans.begin(), ans.end(),nums[i]);
-             if (it != ans.end()) {
-              res.push_back(nums[i]);
-              ans.erase(it);
-             }
+        vector<int>ans(k);
+        for(int i=0;i<k;i++) {
+            ans[i]=nums[i];
         }
-    
-        return res;
+        for(int i=k;i<nums.size();i++){
+            int mini=min_element(ans.begin(),ans.end())-ans.begin();
+            if(ans[mini]<nums[i]){
+                ans.erase(ans.begin()+mini);
+                ans.push_back(nums[i]);
+            }
+        }
+        return ans;
     }
 };
