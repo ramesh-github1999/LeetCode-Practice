f13e484ee841725f8669e950985e4717fc3b4eeb
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-10-31 23:18:30 +0530

    Time: 9 ms (10.07%), Space: 43.2 MB (32.21%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.java
+++ 1035-uncrossed-lines/1035-uncrossed-lines.java
@@ -19,21 +19,12 @@ class Solution {
                 }
                 else {
                     if(nums1[i]==nums2[j]){
-                        int max1=1+dp[i+1][j+1];
-                        int max2=Math.max(dp[i+1][j],dp[i][j+1]);
-                        dp[i][j]=Math.max(max1,max2);
+                       dp[i][j]=1+dp[i+1][j+1];
                     }
-                    else dp[i][j]=Math.max(dp[i+1][j+1],Math.max(dp[i+1][j],dp[i][j+1]));
+                    else dp[i][j]=Math.max(dp[i+1][j],dp[i][j+1]);
                 }
             }
         }
-        int maxi=0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                maxi=Math.max(dp[i][j],maxi);
-            }
-            
-        }
-        return maxi;
+        return dp[0][0];
     }
 }
