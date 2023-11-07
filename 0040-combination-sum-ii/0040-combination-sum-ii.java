aeb8aff8593a5720bf9cd5e838b33fb1defd38db
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-11-07 19:43:12 +0530

    Time: 4 ms (51.31%), Space: 42.4 MB (54.20%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,21 +1,19 @@
 class Solution {
      List<List<Integer>> ans=new ArrayList<>();
-    void fun(int [] arr,List<Integer>l,int i,int target){
+    void fun(int [] arr,List<Integer>l,int idx,int target){
         if(target==0){
-           ans.add(new ArrayList<>(l));
+            ans.add(new ArrayList<>(l));
             return;
         }
-        if(i==arr.length) return;
-        if(arr[i]<=target){
+        if(target<0 || idx==arr.length) return;
+        for (int i = idx; i < arr.length; i++) {
+            if (i > idx && arr[i] == arr[i - 1]) continue;
             l.add(arr[i]);
-            fun(arr,l,i+1,target-arr[i]);
-            l.remove(l.size()-1);
+            fun(arr, l, i + 1, target - arr[i]);
+            l.remove(l.size() - 1);
         }
-        i++;
-        
-        while(i<arr.length && arr[i]==arr[i-1]) i++;
-        fun(arr,l,i,target);
-        
+
+
     }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<Integer> l=new ArrayList<>();
