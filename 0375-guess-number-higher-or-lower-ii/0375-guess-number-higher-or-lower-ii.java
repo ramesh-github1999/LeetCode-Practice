79c173bf94db31bfa6ab15dbb83abf9401ff1b03
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-11-07 13:02:38 +0530

    Time: 14 ms (90.38%), Space: 40.1 MB (75.27%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.java
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.java
@@ -1,17 +1,20 @@
 class Solution {
-     int dp[][];
-    int fun(int l,int h){
-        if(l>=h) return 0;
-        if(dp[l][h]!=0) return dp[l][h];
-        int ans=Integer.MAX_VALUE;
-        for (int i = l; i <=h; i++) {
-            ans=Math.min(ans,Math.max(i+fun(l,i-1),i+fun(i+1,h)));
+    int solveTab(int n){
+        int dp[][]=new int[n+2][n+2];
+        for (int start = n; start >=1; start--) {
+            for (int end = start; end <=n; end++) {
+                if (start==end) continue;
+                int maxi=Integer.MAX_VALUE;
+                for (int i = start; i <=end; i++) {
+                    maxi=Math.min(maxi,Math.max(i+dp[start][i-1],i+dp[i+1][end]));
+                }
+                dp[start][end]=maxi;
+
+            }
         }
-        return dp[l][h]=ans;
+        return dp[1][n];
     }
     public int getMoneyAmount(int n) {
-        dp=new int[n+1][n+1];
-        return fun(1,n);
-
+         return solveTab(n);
     }
 }
