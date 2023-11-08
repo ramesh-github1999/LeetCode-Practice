8761a7e9187893488e493bef50bea3d3a4c4bd7f
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-11-08 18:06:25 +0530

    Time: 6 ms (79.03%), Space: 42.7 MB (52.13%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+         int n=nums.length;
+        if(n==1){
+            if(nums[0]==target || -nums[0]==target) return 1;
+            return 0;
+        }
+        int sum=0;
+        for(int it:nums) sum+=it;
+        if(target>sum) return 0;
+        if((sum+target)%2!=0) return 0;
+        int m=(sum+target)/2;
+        if(m<0) return 0;
+        int t[][]=new int[n+1][m+1];
+        for (int i = 0; i <=n ; i++) {
+            for (int j = 0; j <=m ; j++) {
+                if(j==0 && i==0) t[i][j]=1;
+                else if(i==0) t[i][j]=0;
+                else if(nums[i-1]<=j) t[i][j]=t[i-1][j]+t[i-1][j-nums[i-1]];
+                else t[i][j]=t[i-1][j];
+            }
+        }
+        return t[n][m];
+    }
+}
