6822aae68326a609d6ae3799268e4f0c81361a7b
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-11-09 19:52:30 +0530

    Time: 2 ms (100.00%), Space: 39.7 MB (92.35%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,21 +1,18 @@
 class Solution {
-    public int findTargetSumWays(int[] nums, int target) {
-         int n=nums.length;
-        int sum=0;
-        for(int it:nums) sum+=it;
-        if(target>sum) return 0;
-        if((sum+target)%2!=0) return 0;
-        int m=(sum+target)/2;
-        if(m<0) return 0;
-        int t[][]=new int[n+1][m+1];
-        for (int i = 0; i <=n ; i++) {
-            for (int j = 0; j <=m ; j++) {
-                if(j==0 && i==0) t[i][j]=1;
-                else if(i==0) t[i][j]=0;
-                else if(nums[i-1]<=j) t[i][j]=t[i-1][j]+t[i-1][j-nums[i-1]];
-                else t[i][j]=t[i-1][j];
-            }
-        }
-        return t[n][m];
-    }
+    public int findTargetSumWays(int[] nums, int s) {
+        int sum = 0;
+        for (int n : nums)
+            sum += n;
+        if(s+sum<0) return 0;
+        return sum < s || (s + sum) % 2 > 0 ? 0 : subsetSum(nums, (s + sum) >>> 1); 
+    }   
+
+    public int subsetSum(int[] nums, int s) {
+        int[] dp = new int[s + 1]; 
+        dp[0] = 1;
+        for (int n : nums)
+            for (int i = s; i >= n; i--)
+                dp[i] += dp[i - n]; 
+        return dp[s];
+    } 
 }
