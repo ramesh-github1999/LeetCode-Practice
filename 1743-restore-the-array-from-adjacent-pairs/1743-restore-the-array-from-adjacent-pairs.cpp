caa1f2a04631eb7523432eea9f5af9a553da2c7b
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-11-10 15:50:53 +0530

    Time: 322 ms (88.18%), Space: 142.4 MB (41.21%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
@@ -1,47 +1,32 @@
 class Solution {
 public:
-    /*
-2=>1,3
-1=>2
-3=>4,2
-4=>3
-
-
-*/
-     map<int,vector<int>>mp;
-    set<int>st;
+    unordered_map<int,vector<int>>mp;
     vector<int>ans;
-    void dfs(int i){
-        if(st.size()==mp.size()) return;
-        st.insert(i);
-        ans.push_back(i);
-        vector<int>v=mp[i];
-        for(int i=0;i<v.size();i++){
-            if(st.find(v[i])==st.end()){
-                dfs(v[i]);
+    void dfs(int node,int prev){
+        ans.push_back(node);
+        for(int neighbor:mp[node]){
+            if(neighbor!=prev){
+                dfs(neighbor,node);
             }
         }
     }
+    
     vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
         int n=adjacentPairs.size();
         for(int i=0;i<n;i++){
             mp[adjacentPairs[i][0]].push_back(adjacentPairs[i][1]);
             mp[adjacentPairs[i][1]].push_back(adjacentPairs[i][0]);
         }
-        for(auto it:mp) if(it.second.size()==1){
-            dfs(it.first);
-        }
+        int root=0;
+        for(auto it:mp){
+            if(it.second.size()==1){
+           root=it.first;
+           break;
+        }    
+    
+    }
+        dfs(root,INT_MAX);
         return ans;
             
     }
 };
-
-
-/*
-2=>1,3
-1=>2
-3=>4,2
-4=>3
-
-
-*/
