28e3aaa95b6828174e60c5c49f0638f8f7ddd3c9
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-11-14 23:06:20 +0530

    Time: 7 ms (54.63%), Space: 40.8 MB (65.37%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
@@ -1,34 +1,29 @@
 class Solution {
        
     public double knightProbability(int n, int k, int row, int column) {
-        double curr[][]=new double[n][n];
-        double next[][]=new double[n][n];
-        curr[row][column]=1;
-        int di[]={1,2,1,2,-1,-1,-2,-2};
-        int dj[]={2,1,-2,-1,2,-2,1,-1};
-        for (int step = 0; step <k ; step++) {
+       double dp[][][] = new double[n][n][k + 1];
+        int di[] = {1, 2, 1, 2, -1, -1, -2, -2};
+        int dj[] = {2, 1, -2, -1, 2, -2, 1, -1};
+        dp[row][column][0] = 1;
+        for (int step = 1; step <= k; step++) {
             for (int i = 0; i < n; i++) {
-                for (int j = 0; j <n; j++) {
-                    if(curr[i][j]!=0){
-                        for(int y=0;y<8;y++){
-                            int ni=i+di[y];
-                            int nj=j+dj[y];
-                            if(ni>=0 && ni<n && nj>=0 && nj<n){
-                                next[ni][nj]+=curr[i][j]/8.0;
+                for (int j = 0; j < n; j++) {
+                    if (dp[i][j][step - 1] != 0) {
+                        for (int y = 0; y < 8; y++) {
+                            int ni = i + di[y];
+                            int nj = j + dj[y];
+                            if (ni >= 0 && ni < n && nj >= 0 && nj < n) {
+                                dp[ni][nj][step] += dp[i][j][step - 1] / 8.0;
                             }
                         }
                     }
                 }
             }
-            double next2[][]=new double[n][n];
-            curr=next;
-            next=next2;
-
         }
-        double ans=0;
-        for (int i = 0; i <n ; i++) {
-            for (int j = 0; j <n ; j++) {
-                ans+=curr[i][j];
+        double ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                ans += dp[i][j][k];
             }
         }
         return ans;
