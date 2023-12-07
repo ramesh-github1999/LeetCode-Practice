9469a8f16c9acf69188d2484108247d04e3f355e
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-12-07 14:11:49 +0530

    Time: 2 ms (49.47%), Space: 6.5 MB (6.05%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -4,14 +4,9 @@ public:
         int k=n/7;
         int m=k;
         if(k==0) return (n*(n+1))/2; 
-        int sum=28;
-        int csum=28;
-         
-        while(k>1){
-            csum+=7;
-            sum+=csum;
-            k--;
-        }
+        int f=28;
+        int l=28+(k-1)*7;
+        int sum=(k*(f+l))/2; 
         
         int rem=n%7;
         for(int i=m+1;i<=m+rem;i++) sum+=i;
