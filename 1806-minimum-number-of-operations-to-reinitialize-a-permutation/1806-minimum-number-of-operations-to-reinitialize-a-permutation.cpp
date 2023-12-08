b632f1cd250a7463bfe34bd7f4118016945aa4db
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-12-08 18:29:13 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (90.23%) - LeetHub

--- 1806-minimum-number-of-operations-to-reinitialize-a-permutation/1806-minimum-number-of-operations-to-reinitialize-a-permutation.cpp
+++ 1806-minimum-number-of-operations-to-reinitialize-a-permutation/1806-minimum-number-of-operations-to-reinitialize-a-permutation.cpp
@@ -3,8 +3,7 @@ public:
     int reinitializePermutation(int n) {
         int cnt=0,i=1;
         while(cnt==0|| i>1){
-            if(i<n/2) i=i*2;
-            else i=i*2-(n-1);
+           i=(i*2)%(n-1);
             cnt++;
         }
         return cnt;
