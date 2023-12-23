062b67b5a3adfb25511e0335e2c829bb8d10f470
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2023-12-24 00:42:56 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (62.83%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -2,27 +2,15 @@ class Solution {
 public:
     int maxScore(string s) {
         int n=s.length();
-        vector<int>zero(n,0);
-        vector<int>one(n,0);
-        for(int i=0;i<n;i++){
-            if(i==0){
-                if(s[i]=='0') zero[i]=1;
-            }
-            else if(s[i]=='0') zero[i]=zero[i-1]+1;
-            else zero[i]=zero[i-1];
+       int one=0,zero=0;
+        int maxi=0;
+        for(int i=0;i<n;i++) if(s[i]=='1') one++;
+        for(int i=0;i<n-1;i++){
+            if(s[i]=='1') one--;
+            else zero++;
+            maxi=max(maxi,one+zero);
         }
         
-        for(int i=n-1;i>=0;i--){
-            if(i==n-1){
-                if(s[i]=='1') one[i]=1;
-            }
-            else if(s[i]=='1') one[i]=one[i+1]+1;
-            else one[i]=one[i+1];
-        }
-        int maxi=0;
-        for(int i=0;i<=n-2;i++){
-            maxi=max(maxi,zero[i]+one[i+1]);
-        }
         return maxi;
         
     }
