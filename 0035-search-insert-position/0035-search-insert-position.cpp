846cdbc4e24bd92915db88b7d0ab0240481ca7fc
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2024-01-11 20:42:45 +0530

    Time: 7 ms (29.57%), Space: 10.1 MB (15.11%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -3,16 +3,13 @@ public:
     int searchInsert(vector<int>& nums, int target) {
         int l=0;
         int h=nums.size()-1;
-        int ans=-1;
         while(l<=h){
             int mid=(l+h)/2;
             if(target<=nums[mid]) {
-                ans=mid;
                 h=mid-1;
             }
             else l=mid+1;
         }
-        if(ans==-1) return nums.size();
-        else return ans;
+        return l;
     }
 };
