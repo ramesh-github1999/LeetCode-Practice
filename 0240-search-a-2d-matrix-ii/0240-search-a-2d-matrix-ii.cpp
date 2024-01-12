049124beca532b3913fadfdbd5ed3bdaff6e6870
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2024-01-12 14:48:31 +0530

    Time: 90 ms (63.79%), Space: 15.3 MB (36.29%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -3,14 +3,11 @@ public:
     bool searchMatrix(vector<vector<int>>& a, int t) {
         int n=a.size();
         int m=a[0].size();
-        for(int i=0;i<n;i++){
-            int l=0,h=m-1;
-            while(l<=h){
-                int mid=(l+h)/2;
-                if(a[i][mid]==t) return true;
-                else if(a[i][mid]>t) h=mid-1;
-                else l=mid+1;
-            }
+        int i=0,j=m-1;
+        while(i<n && j>=0){
+            if(a[i][j]==t) return true;
+            else if(a[i][j]>t) j--;
+            else i++;
         }
         return false;
     }
