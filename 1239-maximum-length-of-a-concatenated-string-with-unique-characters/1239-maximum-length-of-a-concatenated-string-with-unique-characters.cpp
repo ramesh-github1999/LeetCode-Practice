6af8af6b934915ebf3ebe5b8f8be638bad0aabc7
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2024-01-23 22:53:55 +0530

    Time: 22 ms (72.49%), Space: 17.3 MB (65.36%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-    int maxi=0;
-    bool check (string s){
-        unordered_map<char,int>mp;
-        for(auto it:s) mp[it]++;
-        return mp.size()==s.size();
-    }
-    void fun(vector<string>& arr, int i,string s){
-        if(i==arr.size()){
-            int k=s.length();
-            maxi=max(k,maxi);
-            return;
+  bool isUnique(string s1, string s2){
+        int a[26] = {0};
+        for(int  i = 0; i<s2.size(); i++){
+            if(a[s2[i]-'a'] > 0) return false;
+            a[s2[i]-'a']++;
         }
-        fun(arr,i+1,s);
-        string t=s;
-        t+=arr[i];
-        if(check(t)) fun(arr,i+1,s+arr[i]);
-        else {
+        for(int i = 0; i<s1.size(); i++){
+            if(a[s1[i]-'a'] > 0) return false;
+        }
+        return true;
+    }
+    int solve(vector<string>& arr,int index,int n,string s){
+        if(index>=n) return s.size();
+        int include=0;
+        int exclude=solve(arr,index+1,n,s);
+        if(isUnique(s,arr[index])){
+            include = solve(arr,index+1,n,s+arr[index]);  
+        }
+        else{
             int k=s.length();
-            maxi=max(k,maxi);
-            return;
+            exclude=max(k,exclude);
         }
         
+        return max(include,exclude);
     }
+    
     int maxLength(vector<string>& arr) {
-       // if(arr.size()==0) return arr[0].length();
-        fun(arr,0,"");
-        return maxi;
+        string s = "";
+        return solve(arr,0,arr.size(),s);
     }
 };
