b280e97d6b4e76e410d3848ae55b40e8f277b0b0
ramesh-github1999 <61752547+ramesh-github1999@users.noreply.github.com>
2024-02-22 13:10:18 +0530

    Time: 87 ms (100.00%), Space: 64.3 MB (69.38%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -4,14 +4,11 @@ public:
             vector<int>dp(n+1,0);
         for (int i=0;i<trust.size();i++){
             dp[trust[i][1]]++;
+             dp[trust[i][0]]--;
         }
-        int ans=-1;
         for (int i=1;i<=n;i++)
-            if(dp[i]==n-1) ans=i;
-        if(ans==-1) return -1;
-        for (int i = 0; i <trust.size() ; i++) {
-            if(trust[i][0]==ans) return -1;
-        }
-        return ans;
+            if(dp[i]==n-1) return i;
+        return -1;
+       
     }
 };
